891985168c97d3fe608a7bd788d398712ce97d85
Ewan Bennett <ewanbennett14@fastmail.com>
2024-07-21 11:19:02 +1000

    Added .bat files for drag and drop on Windows
    Added two batch files that can be used on Windows systems on which direct drag and drop onto Python scripts is broken.
    The paths to the executable and to the scripts will need to be customised to the system.

---
+++ _Hobyah.bat
@@ -0,0 +1,5 @@
+REM This is an example batch file.  Replace the path to the executable
+REM and the path the Python script with paths to where those files are
+REM on your system.
+C:\Users\tester\AppData\Local\Microsoft\WindowsApps\python.exe ^
+  C:\Users\tester\Documents\tech_adm\Hobyah\Hobyah.py  %*
